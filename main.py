3a4b054e6b233e0650dcfa4ce309639829ee9db8
FedotovS <svyatoslavfdtv@yandex.ru>
2023-02-06 21:36:02 +0500

    exercise 2 done. add scale changing with pgup pgdown press

--- main.py
+++ main.py
@@ -3,37 +3,42 @@ import sys
 import requests
 from PyQt5.QtGui import QPixmap
 
-from PyQt5.QtWidgets import QWidget, QMainWindow, QLabel, QApplication
+from PyQt5.QtWidgets import QMainWindow, QLabel, QApplication
 from PyQt5 import uic
+from PyQt5.QtCore import Qt
 
 
 class MainWindow(QMainWindow):
-    pos = "58.310022,51.194357"
-    staticmap_url = "https://static-maps.yandex.ru/1.x/"
-    scale = 19  # 'z' param in api
     map_file = "map.png"
 
     def __init__(self):
         super(MainWindow, self).__init__()
+        self.pos = "58.310022,51.194357"
+        self.staticmap_url = "https://static-maps.yandex.ru/1.x/"
+        self.scale = 19  # 'z' param in api
         self.image = None
         self.pixmap = None
         self.get_image()
         self.loadUI()
 
     def loadUI(self):
-        uic.loadUi("map_v1.ui", self)
+        uic.loadUi("map_v2.ui", self)
         self.setWindowTitle("Большая задача по Maps API. Часть №1")
-        self.pixmap = QPixmap(self.map_file)
         self.image = QLabel(self)
         self.image.move(0, 0)
         self.image.resize(800, 600)
+        self.set_map()
+
+    def set_map(self):
+        self.pixmap = QPixmap(MainWindow.map_file)
         self.image.setPixmap(self.pixmap)
 
     def get_image(self):
-        response = requests.get(url=MainWindow.staticmap_url,
-                                params={"z": MainWindow.scale,
-                                        "ll": MainWindow.pos,
-                                        "l": "map"})
+        response = requests.get(url=self.staticmap_url,
+                                params={"z": self.scale,
+                                        "ll": self.pos,
+                                        "l": "map",
+                                        "size": "650,450"})
         if not response:
             print("Ошибка выполнения запроса:")
             print(response.url)
@@ -45,8 +50,24 @@ class MainWindow(QMainWindow):
         with open(MainWindow.map_file, "wb") as file:
             file.write(response.content)
 
+    def update_image(self):
+        self.get_image()
+        self.set_map()
+        self.update()
+
     def closeEvent(self, event):
         os.remove(MainWindow.map_file)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp:
+            self.scale += 1
+            if self.scale > 23:
+                self.scale = 23
+        elif event.key() == Qt.Key_PageDown:
+            self.scale -= 1
+            if self.scale < 1:
+                self.scale = 1
+        self.update_image()
 
 
 if __name__ == '__main__':
